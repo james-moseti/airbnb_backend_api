34751ad44ba895a444968a6aabc52263f1ccf80b
James Moseti <moseti.james4@gmail.com>
2025-09-14 22:21:15 +0300

    Remove Gunicorn execution command from build script

--- build.sh
+++ build.sh
@@ -6,4 +6,3 @@ pip install --upgrade pip
 pip install -r requirements.txt
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
-exec gunicorn doc_project.wsgi:application --bind 0.0.0.0:$PORT
